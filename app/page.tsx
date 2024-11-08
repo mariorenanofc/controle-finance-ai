53e5c4e0253c8dc20a316d2c92efe363d6e765ff
Mario Renan <mariorenan25@gmail.com>
2024-11-08 07:02:38 -0300

    add: shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
 
   return ( 
   <div className="flex items-center justify-center w-screen">
     <h1 className="text-red-500 p-5">Olá pessoal</h1>
-
+    <Button>Click aqui!</Button>
   </div>
   );
 }
